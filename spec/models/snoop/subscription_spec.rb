b194bd43e97476a8280a18f45e0cee910e67fcb2
Logan Koester <logan@logankoester.com>
2011-12-30 17:32:06 -0500

    Passing subscription specs

--- spec/models/snoop/subscription_spec.rb
+++ spec/models/snoop/subscription_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 module Snoop
   describe Subscription do
+    it { should belong_to(:user) }
+    it { should belong_to(:channel) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
 require 'shoulda/matchers/integrations/rspec'
+require 'database_cleaner'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
@@ -17,4 +18,14 @@ RSpec.configure do |config|
   require 'factory_girl'
   config.include FactoryGirl::Syntax::Methods
   FactoryGirl.find_definitions
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    Snoop::Notification.skip_callback :create, :after, :deliver
+  end
+
+  config.after(:all) do
+    # Clean up data creted in the before(:all) blocks
+    DatabaseCleaner.clean_with :truncation
+  end
 end
